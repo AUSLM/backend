989b9eae93913398684811c1cde3e8d4bedab3a0
kopturovdim <kopturovdim@yandex-team.ru>
2020-08-02 12:50:37 +0300

    small improvements in reset superadmin password

--- reset_superadmin_password.py
+++ reset_superadmin_password.py
@@ -1,19 +1,30 @@
 import requests
-import json
-import sys
+from getpass import getpass
+from argparse import ArgumentParser
 
 
-args = sys.argv
-port = args[1]
-new_password = args[2]
-token = args[3]
+def create_arg_parser():
+    parser = ArgumentParser(description='AUSLM reset superadmin password tool')
+    parser.add_argument('port', metavar='port', type=int, help='localhost port with reset password service')
+    parser.add_argument('token', metavar='token', type=str, help='Auth token')
+    return parser
 
-url = 'http://localhost:' + port + '/api/reset_superadmin_password'
-json_headers = {'Content-type': 'application/json'}
 
-data = {
-    'token': token,
-    'new_password': new_password
-}
+def main():
+    parser = create_arg_parser()
+    args = parser.parse_args()
+    password = getpass('Enter new password: ')
+    if not password:
+        print('New password is empty, do nothing')
+        return
+    url = f'http://localhost:{args.port}/api/reset_superadmin_password'
+    data = {
+        'token': args.token,
+        'new_password': password,
+    }
+    r = requests.post(url, json=data)
+    print(r)
 
-print(requests.post(url, data=json.dumps(data), headers=json_headers).json())
+
+if __name__ == '__main__':
+    main()
